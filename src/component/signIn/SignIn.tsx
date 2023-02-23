a55cba5651306497d5dae8d307001531997ef2a8
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-02-23 16:12:37 +0200

    I added field validation, form verification, authorization function, and a pop-up window with a warning about incorrect input data in the form

--- src/component/signIn/SignIn.tsx
+++ src/component/signIn/SignIn.tsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useDispatch} from "react-redux";
+import {login} from '../../store/AuthSlice';
+import {signInFormClose} from "../../store/SignInModalSlice";
 import {
   Avatar,
   Button,
@@ -10,18 +13,87 @@ import {
   Grid,
   Box,
   Typography,
-  Container
+  Container,
+  Snackbar,
+  Alert, AlertColor
 } from "@mui/material";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 export const SignIn: React.FC = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const [usernameErrorText, setUsernameErrorText] = useState<string>('');
+  const [passwordErrorText, setPasswordErrorText] = useState<string>('');
+
+  const [rememberMe, setRememberMe] = useState<boolean>(true);
+  
+  const [isValid, setIsValid] = useState<boolean>(false);
+
+  const [alertOpen, setAlertOpen] = useState<boolean>(false);
+  const [alertMessage, setAlertMessage] = useState<string>('');
+
+  const [alertSeverity, setAlertSeverity] = useState<AlertColor | undefined>(undefined);
+
+  const dispatch = useDispatch();
+
+  const showAlert = (message: string, severity: AlertColor) => {
+    setAlertMessage(message);
+    setAlertSeverity(severity);
+    setAlertOpen(true);
+  };
+
+  const handleAlertClose = () => {
+    setAlertOpen(false);
+  };
+
+  const handleBlurUsername = (value: string) => {
+    if (!value) {
+      setUsernameErrorText("Це поле не повинне бути порожнім");
+    } else setUsernameErrorText("");
+  };
+
+  const handleChangeUsername = (value: string) => {
+    setUsername(value);
+    handleBlurUsername(value);
+  };
+
+  const handleBlurPassword = (value: string) => {
+    if (!value) {
+      setPasswordErrorText("Це поле не повинне бути порожнім");
+    } else setPasswordErrorText("");
+  };
+
+  const handleChangePassword = (value: string) => {
+    setPassword(value);
+    handleBlurPassword(value);
+  };
+
+  useEffect(() => {
+    if ((username && !usernameErrorText) && (password && !passwordErrorText) && rememberMe) {
+      setIsValid(true);
+    } else setIsValid(false);
+  }, [username, usernameErrorText, password, passwordErrorText, rememberMe]);
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    try {
+      const response = await fetch('/db/users.json');
+      if (response.status === 200) {
+        const users = await response.json();
+        const user = await users.find((item: IUser) => item.username === username && item.password === password);
+        if (user) {
+          dispatch(login({user: user}));
+          setUsername('');
+          setPassword('');
+          dispatch(signInFormClose({signInFormIsOpen: false}));
+        } else showAlert("Ім'я користувача або пароль введено неправильно", "warning");
+      } else if (response.status >= 400 && response.status <= 500) {
+        return new Error("Something went wrong")
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -46,12 +118,17 @@ export const SignIn: React.FC = () => {
             margin="normal"
             required
             fullWidth
-            id="email"
-            label="Email Address"
-            type="email"
-            name="email"
-            autoComplete="email"
+            id="username"
+            label="Username"
+            type="text"
+            name="username"
+            value={username}
+            onChange={(event) => handleChangeUsername(event.target.value)}
+            onBlur={(event) => handleBlurUsername(event.target.value)}
+            onFocus={() => setUsernameErrorText("")}
+            autoComplete="username"
             autoFocus
+            helperText={usernameErrorText}
           />
           <TextField
             margin="normal"
@@ -61,14 +138,23 @@ export const SignIn: React.FC = () => {
             label="Password"
             type="password"
             id="password"
+            value={password}
+            onChange={(event) => handleChangePassword(event.target.value)}
+            onBlur={(event) => handleBlurPassword(event.target.value)}
+            onFocus={() => setPasswordErrorText("")}
             autoComplete="current-password"
+            helperText={passwordErrorText}
           />
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary"/>}
+            control={<Checkbox value="remember" 
+                               checked={rememberMe} 
+                               onChange={(event) => setRememberMe(event.target.checked)}
+                               color="primary"/>}
             label="Remember me"
           />
           <Button
             type="submit"
+            disabled={!isValid}
             fullWidth
             variant="contained"
             sx={{mt: 3, mb: 2}}
@@ -89,6 +175,19 @@ export const SignIn: React.FC = () => {
           </Grid>
         </Box>
       </Box>
+      <Snackbar open={alertOpen}
+                autoHideDuration={3000}
+                onClose={handleAlertClose}
+                anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+                sx={{minWidth: '300px'}}
+      >
+        <Alert onClose={handleAlertClose}
+               severity={alertSeverity}
+               sx={{ width: '100%' }}
+        >
+          {alertMessage}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
