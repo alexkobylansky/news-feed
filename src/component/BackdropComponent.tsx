b1fbf31dc7cddbb1f26fd0f45d7b49b0e7147d23
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-02-28 12:55:07 +0200

    Added Backdrop to SignIn.tsx

---
+++ src/component/BackdropComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Backdrop, CircularProgress} from "@mui/material";
+
+export const BackdropComponent = () => {
+  return (
+    <Backdrop sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}} open>
+      <CircularProgress color="inherit"/>
+    </Backdrop>
+  );
+}
--- src/component/signIn/SignIn.tsx
+++ src/component/signIn/SignIn.tsx
@@ -19,6 +19,7 @@ import {
   AlertColor
 } from "@mui/material";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import {BackdropComponent} from "../BackdropComponent";
 
 export const SignIn: React.FC = () => {
   const navigate = useNavigate();
@@ -37,6 +38,8 @@ export const SignIn: React.FC = () => {
   const [notificationMessage, setNotificationMessage] = useState<string>('');
 
   const [notificationSeverity, setNotificationSeverity] = useState<AlertColor | undefined>(undefined);
+
+  const [getData, setGetData] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -80,6 +83,7 @@ export const SignIn: React.FC = () => {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setGetData(true);
     try {
       const response = await fetch('/db/users.json');
       if (response.status === 200) {
@@ -97,92 +101,96 @@ export const SignIn: React.FC = () => {
       }
     } catch (e) {
       console.log(e);
+    } finally {
+      setGetData(false);
     }
   };
 
+  const backdrop = getData ? <BackdropComponent/> : null;
+
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline/>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-          <LockOutlinedIcon/>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={(event) => handleChangeUsername(event.target.value)}
-            onBlur={(event) => handleBlurUsername(event.target.value)}
-            onFocus={() => setUsernameErrorText("")}
-            autoComplete="username"
-            autoFocus
-            helperText={usernameErrorText}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(event) => handleChangePassword(event.target.value)}
-            onBlur={(event) => handleBlurPassword(event.target.value)}
-            onFocus={() => setPasswordErrorText("")}
-            autoComplete="current-password"
-            helperText={passwordErrorText}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" 
-                               checked={rememberMe} 
-                               onChange={(event) => setRememberMe(event.target.checked)}
-                               color="primary"/>}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            disabled={!isValid}
-            fullWidth
-            variant="contained"
-            sx={{mt: 3, mb: 2}}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+        <CssBaseline/>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+            <LockOutlinedIcon/>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              type="text"
+              name="username"
+              value={username}
+              onChange={(event) => handleChangeUsername(event.target.value)}
+              onBlur={(event) => handleBlurUsername(event.target.value)}
+              onFocus={() => setUsernameErrorText("")}
+              autoComplete="username"
+              helperText={usernameErrorText}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(event) => handleChangePassword(event.target.value)}
+              onBlur={(event) => handleBlurPassword(event.target.value)}
+              onFocus={() => setPasswordErrorText("")}
+              autoComplete="current-password"
+              helperText={passwordErrorText}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember"
+                                 checked={rememberMe}
+                                 onChange={(event) => setRememberMe(event.target.checked)}
+                                 color="primary"/>}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              disabled={!isValid}
+              fullWidth
+              variant="contained"
+              sx={{mt: 3, mb: 2}}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-      <Notification notificationOpen={notificationOpen}
-                    handleNotificationClose={handleNotificationClose}
-                    notificationSeverity={notificationSeverity}
-                    notificationMessage={notificationMessage}/>
-    </Container>
+        <Notification notificationOpen={notificationOpen}
+                      handleNotificationClose={handleNotificationClose}
+                      notificationSeverity={notificationSeverity}
+                      notificationMessage={notificationMessage}/>
+      {backdrop}
+      </Container>
   );
 }
