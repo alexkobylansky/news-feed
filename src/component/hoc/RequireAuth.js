23bec808816591b40124277c84ca97f50ae61961
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-03-01 16:38:32 +0200

    Simplified Navigate props "replace"

--- src/component/hoc/RequireAuth.js
+++ src/component/hoc/RequireAuth.js
@@ -6,6 +6,6 @@ export const RequireAuth = ({children}) => {
   const location = useLocation();
   const user = useSelector(state => state.user.user);
   if (!user) {
-    return <Navigate to="/" replace={true} state={{from: location}}/>
+     return <Navigate to="/" replace state={{from: location}}/>
   } return children
 }
