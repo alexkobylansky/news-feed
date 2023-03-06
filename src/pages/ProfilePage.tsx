696e56abd99b8049b01f820946009b1a8033e6db
Alex Kobylansky <alex.kobylansky@gmail.com>
2023-03-06 18:30:57 +0200

    Created very simple profile paga

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {Box, Typography} from "@mui/material";
+import {AccountCircle} from "@mui/icons-material";
 
-interface ProfilePageProps {
+export const ProfilePage: React.FC = () => {
+  // @ts-ignore
+  const user = useSelector(state => state.user.user);
 
-}
-
-export const ProfilePage: React.FC<ProfilePageProps> = () => {
   return (
-    <div>
-
-    </div>
+    <Box className="profile-page">
+      <Typography component="h1" variant="h2">Profile Page</Typography>
+    <Box component="main">
+      <Box className="profile-info">
+        <Box className="main-info_block">
+          {user.avatar ? <img src={user.avatar} alt={`${user.name} ${user.surname}`} className="user-avatar"/> : <AccountCircle/>}
+          <Typography>Full name: {user.name} {user.surname}</Typography>
+          <Typography>Username: {user.username}</Typography>
+        </Box>
+      </Box>
+    </Box>
+    </Box>
   );
 }
